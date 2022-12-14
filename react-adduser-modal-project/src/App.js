875e89cfaa7ed5de73a6cd4f6fec9992510a80da
MATEKUNA <kuna.mate@parlament.hu>
2022-12-14 11:48:14 +0100

    addUserHandler, userList not refreshing

--- react-adduser-modal-project/src/App.js
+++ react-adduser-modal-project/src/App.js
@@ -1,45 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
-import UserForm from './components/UserForm/UserForm';
 import UserList from './components/UserList/UserList';
 import NewUser from "./components/User/NewUser";
 
-const App = (props) => {
+let defaultUsers = [
+    {
+        "id":1,
+        "name":"Thom Jones",
+        "age": 31
+    },
+    {
+        "id":2,
+        "name":"Adam Smith",
+        "age": 40
+    },
+    {
+        "id":3,
+        "name":"Jake Blaer",
+        "age": 31
+    },
+    {
+        "id":4,
+        "name":"Olivia Reel",
+        "age": 25
+    },
+    {
+        "id":5,
+        "name":"Sylvia Big",
+        "age": 28
+    }
+];
 
-    let defaultUsers = [
-        {
-            "id":1,
-            "name":"Thom Jones",
-            "age": 31
-        },
-        {
-            "id":2,
-            "name":"Adam Smith",
-            "age": 40
-        },
-        {
-            "id":3,
-            "name":"Jake Blaer",
-            "age": 31
-        },
-        {
-            "id":4,
-            "name":"Olivia Reel",
-            "age": 25
-        },
-        {
-            "id":5,
-            "name":"Sylvia Big",
-            "age": 28
-        }
-    ];
-
-    const addUserHandler = (enteredData) => {
-        const userData = {
-            ...enteredData
-        };
-        
-        console.log('Userdata in app: ', userData);
+const App = () => {
+    const [users, setUsers] = useState(defaultUsers);
+    
+    const addUserHandler = user => {
+        setUsers(prevUsers => {
+            return [user, ...users];
+        });
     }
 
     return (
