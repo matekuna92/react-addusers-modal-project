3ba8b6f8a1b776d698a2110d705d6fb89c75d198
matekuna92 <kunamate66@gmail.com>
2022-12-22 13:34:59 +0100

    using useRef hook instead of state and onChange logging for name and age

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Button from '../Button/Button';
 import styles from './UserForm.module.css';
@@ -7,21 +7,27 @@ import Modal from '../Modal/Modal';
 import Wrapper from '../Helpers/Wrapper';
 
 const UserForm = (props) => {
-    const [formUserName, setFormUserName] = useState('');
-    const [formUserAge, setFormUserAge] = useState('');
+    const nameInputRef = useRef();              // create connection between the rendered html element and other JS code
+    const ageInputRef = useRef();               // connect ref with the rendered JSX element, storing the JSX element in the ageInputRef constant
+                                                // this object will always have a 'current' property, which holds its value
     const [error, setError] = useState();       // undefined as initial, no need to pass anything
 
     const formSubmitHandler = (event) => {
         event.preventDefault();
+        console.log(nameInputRef, ageInputRef);
+        console.log('name ref value:', nameInputRef.current.value);
+
+        const enteredName = nameInputRef.current.value;     // storing ref values in constant, we can access them without having to log every keystroke onChange
+        const enteredAge = ageInputRef.current.value;
 
         const userData = {
-            name: formUserName,
-            age: formUserAge
+            name: enteredName,
+            age: enteredAge
         };
 
         console.log(userData);
 
-        if(formUserName.trim().length === 0 || formUserAge.trim().length === 0) {
+        if(enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
             setError({
                 title: 'Invalid input.',
                 message: 'Please enter a valid name and age.'
@@ -30,7 +36,7 @@ const UserForm = (props) => {
         }
 
         // initialised as string, but age is a number - just a conversion to be safe
-        if(+formUserAge < 1) {
+        if(+enteredAge < 1) {
             setError({
                 title: 'Invalid age.',
                 message: 'Age must be a positive number.'
@@ -39,19 +45,7 @@ const UserForm = (props) => {
             return;
         }
 
-        setFormUserName('');
-        setFormUserAge('');
-        console.log(formUserName, formUserAge);
-
         props.onSaveUser(userData);
-    }
-
-    const nameChangeHandler = (event) => {
-        setFormUserName(event.target.value);
-    }
-
-    const ageChangeHandler = (event) => {
-        setFormUserAge(event.target.value);
     }
 
     // change Error object from object to null, so modal wont be rendered, when backdrop or Okay button is clicked
@@ -65,9 +59,9 @@ const UserForm = (props) => {
             <Card className={styles['user-form']}>
                 <form onSubmit={formSubmitHandler}>
                     <label htmlFor='username'>Username</label>
-                    <input id='username' type='text' value={formUserName} onChange={nameChangeHandler} />
+                    <input id='username' type='text' ref={nameInputRef} />
                     <label htmlFor='age'>Age(Years)</label>
-                    <input id='age' type='number' value={formUserAge} onChange={ageChangeHandler} />
+                    <input id='age' type='number' ref={ageInputRef} />
                     <Button type='submit'> Add User </Button>
                 </form>
             </Card>
